8f25ab7038b99a5792d8b9dcab961c1b477873d6
Cole Vita <106690201+cole-vita-unc@users.noreply.github.com>
2025-03-06 18:01:42 -0500

    Update identity

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,5 +1,5 @@
-export const OWNER_NAME: string = `[OWNER NAME]`;
-export const OWNER_DESCRIPTION: string = `[OWNER DESCRIPTION]`;
+export const OWNER_NAME: string = `Shoppy`;
+export const OWNER_DESCRIPTION: string = `your very own AI shopping assistant for clothes from Nike, Lululemon, Princess Polly, Patagonia, White Fox, and more. Start by asking me about a specific item you want or brand!`;
 
 export const AI_NAME: string = `[AI NAME]`;
 export const AI_TONE: string = `[AI TONE]`;
